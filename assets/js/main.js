ca272581b9f20f2126db85ea7f40d5b651027741
tuannc <nctuan.dev@gmail.com>
2023-05-01 23:32:01 +0700

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-goongjs.accessToken = 'ZDxB8PiJAYaRkOfxfpg6D3flfT0d2c61Dd5Ehro6';
-const  mapbox = {
-    accessToken:'pk.eyJ1IjoidHVhbm5jcyIsImEiOiJjbGdtaTNzejQwNjBoM21tbXczbmt2N2U0In0.uIj52PPu5Raps5IcD2REqg'
-}
-var myMap = new MyMap();
-myMap.init();
+goongjs.accessToken = 'ZDxB8PiJAYaRkOfxfpg6D3flfT0d2c61Dd5Ehro6'; // key goong map
+var myMap = new MyMap(); // khởi tạo map
+myMap.init(); // gọi hàm init
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,37 +1,46 @@
 class MyMap {
 	constructor() {
-		this.map = null;
-		this.isStartDirection = false;
+		this.map = null; // biến lưu trữ map
+		this.isStartDirection = false; // biến kiểm tra trạng thái bắt đầu tìm đường
 		this.goongClient = goongSdk({
-			accessToken: "fB2uKvZElzJ2NNaJFuGPhwJAlzf3N5RMMZ4sQiif",
+			accessToken: "fB2uKvZElzJ2NNaJFuGPhwJAlzf3N5RMMZ4sQiif", // key goong map
 		});
-		this.CurrLocation = null;
+		this.CurrLocation = null; // biến lưu trữ vị trí hiện tại
 	}
 
-	showMyLocation(lng,lat) {
-		$(".myLocation").show();
-		$(".myLocation .lng").text(lng);
-		$(".myLocation .lat").text(lat);
+	showMyLocation(lng, lat) {
+		$(".myLocation").show(); // hiển thị vị trí hiện tại
+		$(".myLocation .lng").text(lng); // hiển thị kinh độ
+		$(".myLocation .lat").text(lat); // hiển thị vĩ độ
 	}
 
 	init() {
 		getLocation((position) => {
 			this.map = new goongjs.Map({
-				container: "map", // container id
-				style: "https://tiles.goong.io/assets/goong_map_web.json", //stylesheet location
-				center: [position.coords.longitude, position.coords.latitude], // starting position
-				zoom: 15, // starting zoom
+				container: "map", // id của thành phần chứa map
+				style: "https://tiles.goong.io/assets/goong_map_web.json", // link json style
+				center: [position.coords.longitude, position.coords.latitude], // vị trí hiện tại
+				zoom: 15, // level zoom hiện tại
 			});
-			this.map.on("load", () => {
-				this.addMarker(position.coords.longitude, position.coords.latitude);
-				this.addControl();
+			this.map.on("load", () => { // sự kiện load map
+				this.addMarker(
+					position.coords.longitude,
+					position.coords.latitude
+				); // thêm marker vị trí hiện tại
+				this.addControl(); // thêm control
 			});
-			this.showMyLocation(position.coords.longitude, position.coords.latitude);
-			this.CurrLocation = [position.coords.longitude, position.coords.latitude]
+			this.showMyLocation(
+				position.coords.longitude, // hiển thị vị trí hiện tại
+				position.coords.latitude
+			);
+			this.CurrLocation = [
+				position.coords.longitude,
+				position.coords.latitude,
+			]; // lưu trữ vị trí hiện tại
 		});
 	}
 	addControl() {
-		this.map.addControl(new goongjs.ScaleControl(), "bottom-right");
+		this.map.addControl(new goongjs.ScaleControl(), "bottom-right"); // thêm control scale
 		// Add geolocate control to the map.
 		this.map.addControl(
 			new goongjs.GeolocateControl({
@@ -41,40 +50,42 @@ class MyMap {
 				trackUserLocation: true,
 			}),
 			"bottom-right"
-		);
-		this.map.addControl(new goongjs.NavigationControl(), "bottom-right");
-		this.map.addControl(new goongjs.FullscreenControl());
+		); // thêm control vị trí hiện tại
+		this.map.addControl(new goongjs.NavigationControl(), "bottom-right"); // thêm control điều hướng
+		this.map.addControl(new goongjs.FullscreenControl()); // thêm control full màn hình
 		// Add the control to the map.
 
 		this.autoComplete = new GoongGeocoder({
-			accessToken: "fB2uKvZElzJ2NNaJFuGPhwJAlzf3N5RMMZ4sQiif",
+			accessToken: "fB2uKvZElzJ2NNaJFuGPhwJAlzf3N5RMMZ4sQiif", // key goong map
 			goongjs: goongjs,
 			width: "100%",
-		});
+		}); // tạo biến tìm kiếm địa chỉ với key goong map
 
-		this.map.addControl(this.autoComplete, "top-left");
-		this.autoComplete.on("result", (e) => {
+		this.map.addControl(this.autoComplete, "top-left"); // thêm control tìm kiếm địa chỉ
+		this.autoComplete.on("result", (e) => { // sự kiện tìm kiếm địa chỉ
 			console.log(e);
 
-			$(".display-info .title-header").text(e.result.result.name);
+			$(".display-info .title-header").text(e.result.result.name); // hiển thị tên địa chỉ
 			$(".display-info .addressDetail .address").text(
 				e.result.result.formatted_address
-			);
-			$("input[name='destination']").val(`${e.result.result.geometry.location.lat},${e.result.result.geometry.location.lng}`);
-			$(".display-info").show();
+			); // hiển thị địa chỉ
+			$("input[name='destination']").val(
+				`${e.result.result.geometry.location.lat},${e.result.result.geometry.location.lng}`
+			); // lưu trữ vị trí địa chỉ
+			$(".display-info").show(); // hiển thị thông tin địa chỉ
 		});
-		this.autoComplete.on("clear", (e) => {
-			if (!e) {
-				$(".display-info").hide();
-				this.removeSource()
-				this.isStartDirection = false;
+		this.autoComplete.on("clear", (e) => { // sự kiện xóa tìm kiếm địa chỉ
+			if (!e) { // nếu không có sự kiện
+				$(".display-info").hide(); // ẩn thông tin địa chỉ
+				this.removeSource(); // xóa source
+				this.isStartDirection = false; // trạng thái tìm đường = false
 			}
 		});
-		this.autoComplete.on("loading", (e) => {
+		this.autoComplete.on("loading", (e) => { // sự kiện tìm kiếm địa chỉ
 			console.log(e);
 		});
 		// current location
-		this.directions = new MapboxDirections({
+		this.directions = new MapboxDirections({ // tạo biến tìm đường
 			goongjs: goongjs,
 			accessToken: goongjs.accessToken,
 			unit: "metric",
@@ -86,53 +97,33 @@ class MyMap {
 			},
 			placeholderOrigin: "Chọn điểm đi",
 			placeholderDestination: "Chọn điểm đến",
-		});
-		this.directions.on("route", (e) => {
-			console.log(e);
-		});
-
-		this.directions.on("profile", (e) => {
-			console.log(e);
-		});
-
-		this.directions.on("clear", (e) => {
-			console.log(e);
-		});
-
-		this.directions.on("loading", (e) => {
-			console.log(e);
-		});
-
-		this.directions.on("origin", (e) => {
-			console.log(e);
-		});
-
-		this.directions.on("destination", (e) => {
-			console.log(e);
-		});
-
-		this.directions.on("error", (e) => {
-			console.log(e);
-		});
+		}); // tạo biến tìm đường
 		// add control find location
-		this.map.addControl(this.directions, "top-left");
-		this.btnDirection()
+		this.map.addControl(this.directions, "top-left"); // thêm control tìm đường
+		this.btnDirection(); // gán sự kiện cho nút nhấn tìm đường
 		getLocation((position) => {
-			$("input[name='origin']").val(`${position.coords.latitude},${position.coords.longitude}`)
-			console.log(`${position.coords.latitude},${position.coords.longitude}`)
-			this.marker.setLngLat([position.coords.longitude, position.coords.latitude])
+			$("input[name='origin']").val(
+				`${position.coords.latitude},${position.coords.longitude}`
+			); // lưu trữ vị trí hiện tại
+			console.log(
+				`${position.coords.latitude},${position.coords.longitude}`
+			); // log vị trí hiện tại
+			this.marker.setLngLat([
+				position.coords.longitude,
+				position.coords.latitude,
+			]); // thêm marker vị trí hiện tại
 		});
-		this.autoUpdateLocation()
+		this.autoUpdateLocation(); // tự động cập nhật vị trí hiện tại
 
 		// add control find location
 	}
 	addMarker(lng, lat) {
-		this.marker = new goongjs.Marker().setLngLat([lng, lat])
-		this.marker.addTo(this.map)
+		this.marker = new goongjs.Marker().setLngLat([lng, lat]); // tạo marker
+		this.marker.addTo(this.map); // thêm marker vào map
 	}
-	StartDirection(origin,destination) {
+	StartDirection(origin, destination) {
 		var layers = this.map.getStyle().layers;
-		
+
 		// Find the index of the first symbol layer in the map style
 		var firstSymbolId;
 		for (var i = 0; i < layers.length; i++) {
@@ -141,25 +132,25 @@ class MyMap {
 				break;
 			}
 		}
-		const MyMapT = this;
-		this.goongClient.directions
+		const MyMapT = this; // gán this cho biến khác
+		this.goongClient.directions // tìm đường
 			.getDirections({
 				origin: origin,
 				destination: destination,
 				vehicle: "car",
-			})
-			.send()
-			.then(function (response) {
-				var directions = response.body;
-				var route = directions.routes[0];
-				var geometry_string = route.overview_polyline.points;
-				var geoJSON = polyline.toGeoJSON(geometry_string);
-				MyMapT.map.addSource("route", {
+			}) // tìm đường
+			.send() // gửi yêu cầu
+			.then(function (response) { // trả về kết quả
+				var directions = response.body; // lấy kết quả
+				var route = directions.routes[0]; // lấy kết quả
+				var geometry_string = route.overview_polyline.points; // lấy kết quả
+				var geoJSON = polyline.toGeoJSON(geometry_string); // tạo geojson
+				MyMapT.map.addSource("route", { // thêm source
 					type: "geojson",
 					data: geoJSON,
 				});
-				console.log(geoJSON)
-				addDoc(geoJSON)
+				console.log(geoJSON); // log kết quả
+				addDoc(geoJSON); // thêm vào firestore
 				// Add route layer below symbol layers
 				MyMapT.map.addLayer(
 					{
@@ -176,48 +167,65 @@ class MyMap {
 						},
 					},
 					firstSymbolId
-				);
+				); // thêm layer
 			});
 	}
 
 	btnDirection() {
-		$(".btn-direction").click(() => {
-			this.removeSource()
-			var origin = $("input[name='origin']").val();
-			var destination = $("input[name='destination']").val();
-			this.map.flyTo({center:origin.split(',').reverse(),essential: true})
-			$(".display-info").hide();
-			this.StartDirection(origin,destination);
-			this.isStartDirection = true;
+		$(".btn-direction").click(() => { // sự kiện nhấn nút tìm đường
+			this.removeSource(); // xóa source
+			var origin = $("input[name='origin']").val(); // lấy vị trí hiện tại
+			var destination = $("input[name='destination']").val(); // lấy vị trí đích
+			this.map.flyTo({ // di chuyển đến vị trí đích
+				center: origin.split(",").reverse(), // tọa độ
+				essential: true, // nếu false thì sẽ không có hiệu ứng
+			});
+			$(".display-info").hide(); // ẩn thông tin địa chỉ
+			this.StartDirection(origin, destination); // tìm đường
+			this.isStartDirection = true; // trạng thái tìm đường = true
 		});
 	}
 
 	autoUpdateLocation() {
 		setInterval(() => {
 			getLocation((position) => {
-				$("input[name='origin']").val(`${position.coords.latitude},${position.coords.longitude}`)
-				this.showMyLocation(position.coords.latitude,position.coords.longitude)
-				console.log(`${position.coords.latitude},${position.coords.longitude}`)
-				this.marker.setLngLat([position.coords.longitude, position.coords.latitude])
-				var origin = $("input[name='origin']").val();
-				var destination = $("input[name='destination']").val();
-				console.log(`${this.CurrLocation[0]},${this.CurrLocation[1]}`,origin)
-				if(this.isStartDirection && `${this.CurrLocation[1]},${this.CurrLocation[0]}` != origin)
-				{
-					this.removeSource()
-					this.StartDirection(origin,destination);
+				$("input[name='origin']").val(
+					`${position.coords.latitude},${position.coords.longitude}`
+				); // lưu trữ vị trí hiện tại
+				this.showMyLocation(
+					position.coords.latitude,
+					position.coords.longitude
+				); // câp nhật vị trí hiện tại
+				console.log(
+					`${position.coords.latitude},${position.coords.longitude}`
+				); // log vị trí hiện tại
+				this.marker.setLngLat([
+					position.coords.longitude,
+					position.coords.latitude,
+				]); // thêm marker vị trí hiện tại
+				var origin = $("input[name='origin']").val(); // lấy vị trí hiện tại
+				var destination = $("input[name='destination']").val(); // lấy vị trí đích
+				console.log(
+					`${this.CurrLocation[0]},${this.CurrLocation[1]}`,
+					origin
+				); // log vị trí hiện tại
+				if (
+					this.isStartDirection &&
+					`${this.CurrLocation[1]},${this.CurrLocation[0]}` != origin
+				) { // nếu đang tìm đường và vị trí hiện tại khác vị trí hiện tại
+					this.removeSource(); // xóa đường
+					this.StartDirection(origin, destination); // tìm đường mới
 				}
 			});
-		}, 5000);
+		}, 5000); // thời gian cập nhật vị trí hiện tại (ms) mỗi 5 giây cập nhật 1 lần
 	}
 
-	removeSource(){
-		if(this.map.getLayer("route")){
-			this.map.removeLayer("route")
+	removeSource() {
+		if (this.map.getLayer("route")) { // nếu có layer route
+			this.map.removeLayer("route"); // xóa layer route
 		}
-		if (this.map.getSource("route")){
-			this.map.removeSource("route");
-		 }
+		if (this.map.getSource("route")) { // nếu có source route
+			this.map.removeSource("route"); // xóa source route
+		}
 	}
-	
 }
