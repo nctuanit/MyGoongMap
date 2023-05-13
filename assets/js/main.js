8f165076c509f0c5621b371e814fd1d84c3d539c
LHU AI LAB <lhu_ai_lab_mac_air_2@AI-MacBook-Air2.local>
2023-05-13 13:29:44 +0700

    h

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 goongjs.accessToken = 'ZDxB8PiJAYaRkOfxfpg6D3flfT0d2c61Dd5Ehro6'; // key goong map
 var myMap = new MyMap(); // khởi tạo map
 myMap.init(); // gọi hàm init
+window.addEventListener('message', event => {
+	console.log(event.data);
+	localStorage.setItem('location', event.data);
+})
